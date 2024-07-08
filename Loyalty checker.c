4e5d3dc7854b6916f79a0194bfaa8aab0efe5624
Coding of Geograpikals chanel <161205801+PappyGio@users.noreply.github.com>
2024-07-08 13:51:36 +0800

    Update Loyalty checker.c

--- Loyalty checker.c
+++ Loyalty checker.c
@@ -1,113 +1,113 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
 
-#define STRING_MAX 50
-#define LIST_MAX 10
+// #define STRING_MAX 50
+// #define LIST_MAX 10
 
-typedef struct {
-    int month;
-    int year;
-} currentDate;
+// typedef struct {
+//     int month;
+//     int year;
+// } currentDate;
 
-typedef struct {
-    int hireMonth;
-    int hireYear;
-} hiringDetails;
+// typedef struct {
+//     int hireMonth;
+//     int hireYear;
+// } hiringDetails;
 
-typedef struct {
-    char fName[STRING_MAX];
-} empDetails;
+// typedef struct {
+//     char fName[STRING_MAX];
+// } empDetails;
 
-typedef struct {
-    hiringDetails dateOfHire;
-    empDetails employeeInfo;
-} employee;
+// typedef struct {
+//     hiringDetails dateOfHire;
+//     empDetails employeeInfo;
+// } employee;
 
-typedef struct {
-    employee employeeList[LIST_MAX];
-    int count;
-} employeeRecords;
+// typedef struct {
+//     employee employeeList[LIST_MAX];
+//     int count;
+// } employeeRecords;
 
-employee* getLoyalList(employeeRecords record, int *loyalListCount, currentDate currDate);
+// employee* getLoyalList(employeeRecords record, int *loyalListCount, currentDate currDate);
 
-void display(employee *loyalList, int loyalListCount);
+// void display(employee *loyalList, int loyalListCount);
 
-int main() {
-    employeeRecords record;
-    int employeeCount;
-    int loyalListCount = 0;
-    record.count = 0;
-    employee *loyalList;
-    currentDate date = {7, 2023}; // Change this date for different outputs
+// int main() {
+//     employeeRecords record;
+//     int employeeCount;
+//     int loyalListCount = 0;
+//     record.count = 0;
+//     employee *loyalList;
+//     currentDate date = {7, 2023}; // Change this date for different outputs
     
-    int i = 0;
-    printf("Input Number of Employees: ");
-    scanf("%d", &employeeCount);
+//     int i = 0;
+//     printf("Input Number of Employees: ");
+//     scanf("%d", &employeeCount);
     
-    printf("\nInput Details for Employees:\n");
-    for (i = 0; i < employeeCount; i++) {
-        printf("\n===Employee %d===\n", i + 1);
-        printf("Input First Name: ");
-        scanf("%s", record.employeeList[i].employeeInfo.fName);
-        printf("Input Month of Hire: ");
-        scanf("%d", &record.employeeList[i].dateOfHire.hireMonth);
-        printf("Input Year of Hire: ");
-        scanf("%d", &record.employeeList[i].dateOfHire.hireYear);
-        record.count++;
-    }
+//     printf("\nInput Details for Employees:\n");
+//     for (i = 0; i < employeeCount; i++) {
+//         printf("\n===Employee %d===\n", i + 1);
+//         printf("Input First Name: ");
+//         scanf("%s", record.employeeList[i].employeeInfo.fName);
+//         printf("Input Month of Hire: ");
+//         scanf("%d", &record.employeeList[i].dateOfHire.hireMonth);
+//         printf("Input Year of Hire: ");
+//         scanf("%d", &record.employeeList[i].dateOfHire.hireYear);
+//         record.count++;
+//     }
     
-    loyalList = getLoyalList(record, &loyalListCount, date);
+//     loyalList = getLoyalList(record, &loyalListCount, date);
     
-    display(loyalList, loyalListCount);
+//     display(loyalList, loyalListCount);
     
-    return 0;
-}
+//     return 0;
+// }
 
-employee* getLoyalList(employeeRecords record, int *loyalListCount, currentDate currDate) {
-    static employee loyalList[LIST_MAX];
-    *loyalListCount = 0;
+// employee* getLoyalList(employeeRecords record, int *loyalListCount, currentDate currDate) {
+//     static employee loyalList[LIST_MAX];
+//     *loyalListCount = 0;
     
-    for (int i = 0; i < record.count; i++) {
-        // Check if the employee has been with the company for 5 consecutive years
-        if (currDate.year - record.employeeList[i].dateOfHire.hireYear >= 5) {
-            // Additional check for exact 5 years in the same month or earlier
-            if (currDate.year - record.employeeList[i].dateOfHire.hireYear == 5) {
-                if (record.employeeList[i].dateOfHire.hireMonth <= currDate.month) {
-                    loyalList[*loyalListCount] = record.employeeList[i];
-                    (*loyalListCount)++;
-                }
-            } else { // More than 5 years
-                loyalList[*loyalListCount] = record.employeeList[i];
-                (*loyalListCount)++;
-            }
-        }
-    }
+//     for (int i = 0; i < record.count; i++) {
+//         // Check if the employee has been with the company for 5 consecutive years
+//         if (currDate.year - record.employeeList[i].dateOfHire.hireYear >= 5) {
+//             // Additional check for exact 5 years in the same month or earlier
+//             if (currDate.year - record.employeeList[i].dateOfHire.hireYear == 5) {
+//                 if (record.employeeList[i].dateOfHire.hireMonth <= currDate.month) {
+//                     loyalList[*loyalListCount] = record.employeeList[i];
+//                     (*loyalListCount)++;
+//                 }
+//             } else { // More than 5 years
+//                 loyalList[*loyalListCount] = record.employeeList[i];
+//                 (*loyalListCount)++;
+//             }
+//         }
+//     }
     
-    // Allocate exact memory for loyalList based on loyalListCount
-    employee *exactLoyalList = (employee*) malloc((*loyalListCount) * sizeof(employee));
-    if (exactLoyalList == NULL) {
-        printf("Memory allocation failed.\n");
-        exit(1);
-    }
-    memcpy(exactLoyalList, loyalList, (*loyalListCount) * sizeof(employee));
+//     // Allocate exact memory for loyalList based on loyalListCount
+//     employee *exactLoyalList = (employee*) malloc((*loyalListCount) * sizeof(employee));
+//     if (exactLoyalList == NULL) {
+//         printf("Memory allocation failed.\n");
+//         exit(1);
+//     }
+//     memcpy(exactLoyalList, loyalList, (*loyalListCount) * sizeof(employee));
     
-    return exactLoyalList;
-}
+//     return exactLoyalList;
+// }
 
-void display(employee *loyalList, int loyalListCount) {
-    if (loyalListCount > 0) {
-        printf("\nLoyal Employees List:\n");
-        for (int i = 0; i < loyalListCount; i++) {
-            printf("Name: %s -- Hire Date: %d-%d\n",
-                   loyalList[i].employeeInfo.fName,
-                   loyalList[i].dateOfHire.hireMonth,
-                   loyalList[i].dateOfHire.hireYear);
-        }
-    } else {
-        printf("\nThere are currently no Employees who have been with the company for 5 consecutive years.\n");
-    }
-}
+// void display(employee *loyalList, int loyalListCount) {
+//     if (loyalListCount > 0) {
+//         printf("\nLoyal Employees List:\n");
+//         for (int i = 0; i < loyalListCount; i++) {
+//             printf("Name: %s -- Hire Date: %d-%d\n",
+//                    loyalList[i].employeeInfo.fName,
+//                    loyalList[i].dateOfHire.hireMonth,
+//                    loyalList[i].dateOfHire.hireYear);
+//         }
+//     } else {
+//         printf("\nThere are currently no Employees who have been with the company for 5 consecutive years.\n");
+//     }
+// }
 
 
 /*
@@ -185,17 +185,110 @@ Input Month of Hire: 10
 Input Year of Hire: 2019
 There are currently no Employees who are eligible for the reward.
 main.c
-
-
-C
-C
-
-
-    
-
-
-  
-
-
-
 */
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define STRING_MAX 50
+#define LIST_MAX 10
+typedef struct{
+	int month;
+	int year;
+}currentDate;
+typedef struct{
+	int hireMonth;
+	int hireYear;
+}hiringDetails;
+
+typedef struct{
+	char fName[STRING_MAX];
+}empDetails;
+
+typedef struct{
+	hiringDetails dateOfHire;
+	empDetails employeeInfo;
+}employee;
+
+typedef struct{
+	employee employeeList[LIST_MAX];
+	int count;
+}employeeRecords;
+
+employee* getLoyalList(employeeRecords record,int *loyalListCount,currentDate currDate);
+
+void display(employee *loyalList,int loyalListCount);
+int main(){
+	employeeRecords record;
+	int employeeCount;
+	int loyalListCount=0;
+	record.count=0;
+	employee *loyalList;
+	currentDate date={07,2023};
+	
+	int i=0;
+	printf("Input Number of Employees:" );
+    scanf("%d",&employeeCount);
+    
+    printf("\nInput Details for Employees: \n");
+    for(i=0;i<employeeCount;i++){
+    	printf("\n\n===Employee %d===\n",i+1);
+    	/*CODE FOR INPUTS*/
+    	printf("Input First Name: ");
+    	scanf("%s", record.employeeList[i].employeeInfo.fName);
+    	
+    	printf("Input Month of Hire: ");
+    	scanf("%d", &record.employeeList[i].dateOfHire.hireMonth);
+    	
+    	printf("Input Year of Hire: ");
+    	scanf("%d", &record.employeeList[i].dateOfHire.hireYear);
+    	
+    	record.count++;
+	}
+	
+	loyalList=getLoyalList(record,&loyalListCount,date);
+	
+	display(loyalList, loyalListCount);
+	
+}
+
+employee* getLoyalList(employeeRecords record,int *loyalListCount,currentDate currDate){
+    /*CODE TO CREATE THE LOYAL LIST*/
+    int i;
+    employee *workers = (employee*)malloc(sizeof(employee) * record.count);
+    int count = 0;
+    
+    for(i = 0; i < record.count; i++){
+        if((record.employeeList[i].dateOfHire.hireMonth <= currDate.month && 
+        record.employeeList[i].dateOfHire.hireYear <= (currDate.year - 5)) ||
+        (record.employeeList[i].dateOfHire.hireMonth >= currDate.month && 
+        record.employeeList[i].dateOfHire.hireYear <= (currDate.year - 6))){
+            *loyalListCount += 1;
+            workers[count++] = record.employeeList[i];
+        }
+    }
+    
+	
+	return workers;
+}
+
+void display(employee *loyalList,int loyalListCount){
+    /*CODE TO DISPLAY*/
+    int i;
+    
+    if(loyalListCount == 0){
+        printf("There are currently no Employees who are eligible for the reward.");
+        return;
+    }
+    
+    printf("Loyal List:\n\n");
+    for(i = 0; i < loyalListCount; i++){
+        printf("Name: %s -- ", loyalList[i].employeeInfo.fName);
+        printf("Hire Date:%d-%d\n", loyalList[i].dateOfHire.hireMonth, 
+        loyalList[i].dateOfHire.hireYear);
+    }
+}
